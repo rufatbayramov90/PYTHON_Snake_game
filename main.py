fa4b69d86d955dbf52e3eb3716e594f87890d5c2
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-03 15:05:29 +0400

    snake biting its own tail

--- main.py
+++ main.py
@@ -36,6 +36,16 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
 
 
 
